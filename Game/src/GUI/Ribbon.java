1eb70ad30f9720881d6eb5a466c238076d7a532a
Admin <natpworkspace@gmail.com>
2025-04-08 15:05:09 +0200

    /

--- Game/src/GUI/Ribbon.java
+++ Game/src/GUI/Ribbon.java
@@ -1,4 +1,32 @@
 package GUI;
 
-public class Ribbon {
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+public class Ribbon extends AnchorPane {
+    ImageView body;
+    ImageView material;
+    int amount = 100;
+
+    Text text = new Text();
+
+    public Ribbon(Image body, Image material, int amount) {
+        this.body = new ImageView(body);
+        this.material = new ImageView(material);
+        this.amount = amount;
+        text.setText(String.valueOf(amount));
+        getChildren().addAll(this.body, this.material, text);
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void update (int amountChange){ //updates the amount of gathered recources (bigger/smaller)
+        amount += amountChange;
+        text.setText(String.valueOf(amount));
+    }
+
 }
