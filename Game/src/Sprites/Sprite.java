26ced587e9e69ee90da0365d470e9e51d385d767
makar <makary.galka@gmail.com>
2025-04-16 14:06:30 +0200

    bf update

--- Game/src/Sprites/Sprite.java
+++ Game/src/Sprites/Sprite.java
@@ -1,42 +1,37 @@
 package Sprites;
 
-
-import Client.Settings;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-public class Sprite {
+public class Sprite extends ImageView{
     private int number = 0;
-    private int row;
-    private int column;
+    private int y;
+    private int x;
     private int hp;
     private double width;
     private double height;
 
     private Type type;
 
-    public Sprite(int hp, int row, int column, double width, double heigth) {
-        this.column = column;
-        this.row = row;
+    public Sprite(int hp, int x, int y, double width, double heigth) {
+        this.x = x;
+        this.y = y;
         this.hp = hp;
         this.width = width;
         this.height = heigth;
     }
 
 
+    public void move(double dx, double dy) {
+        setLayoutX(getLayoutX() + dx);
+        setLayoutY(getLayoutY() + dy);
+        x += dx;
+        y += dy;
+    }
+
+
     public Type getType() {return type;}
-
-    public int getRow() {
-        return row;
-    }
-
-    public int getColumn() {
-        return column;
-    }
-
     public int getNumber() {
         return number;
     }
-
 
 }
--- Game/src/Sprites/Tree.java
+++ Game/src/Sprites/Tree.java
@@ -5,11 +5,9 @@ import javafx.scene.image.ImageView;
 
 public class Tree extends Sprite {
     public static Images tree = Images.valueOf("Tree");
-    public static SingleAnimatedSprite singleAnimatedSprite;
 
     public Tree(int row, int column, int hp, ImageView imageView, double width, double heigth) {
         super(row, column, hp, width, heigth);
-        singleAnimatedSprite = new SingleAnimatedSprite(row, column, hp, width, heigth, tree.toString(), 5, true);
     }
 
     public static void addTrees(int row, int column) {
