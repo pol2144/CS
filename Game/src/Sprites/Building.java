8b7dfe1669eedd752834a1c725eef4b3bf9feb13
makar <makary.galka@gmail.com>
2025-04-23 15:02:06 +0200

    building

---
+++ Game/src/Sprites/Building.java
@@ -0,0 +1,29 @@
+package Sprites;
+
+import javafx.scene.image.ImageView;
+
+public class Building extends ImageView {
+    private int number = 0;
+    private int y;
+    private int x;
+    private int hp;
+    private double width;
+    private double height;
+
+    public Building(int x, int y, int hp, double width, double height) {
+        this.x = x;
+        this.y = y;
+        this.hp = hp;
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Building" + "," + x + "," + y + "," + hp + "," + width + "," + height;
+    }
+
+    public static Building Building(String string) {
+        return new Building(Integer.parseInt(string.split(",")[1]), Integer.parseInt(string.split(",")[2]), Integer.parseInt(string.split(",")[3]), Double.parseDouble(string.split(",")[4]), Double.parseDouble(string.split(",")[5]));
+    }
+}
